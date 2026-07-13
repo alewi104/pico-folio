5e384f1afb04f26aca48ba4c72ca4460664996fa
unknown <alewi104@asu.edu>
2026-07-13 17:55:01 -0400

    Revert "NavBar redesign"
    This reverts commit f3bc783aa3bce52c742c6e1de6b6134c53337dab.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,64 @@
-import "./NavBar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import nav from "react-bootstrap/Navbar";
 
 function NavBar() {
   return (
-    <header className="navbar">
-      <div className="container navbar-container">
-        <a href="#" className="logo">
-          <span className="logo-name">Ahlaireah Lewis</span>
-          <span className="logo-title">Software Engineer</span>
-        </a>
-
-        <nav>
-          <ul className="nav-links">
-            <li>
-              <a href="#">Home</a>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"></a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
             </li>
-
-            <li>
-              <a href={import.meta.env.BASE_URL + "/projects"}>Projects</a>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href={import.meta.env.BASE_URL + "/projects"}
+              >
+                Projects
+              </a>
             </li>
-
-            <li>
-              <a href={import.meta.env.BASE_URL + "/resume"}>Resume</a>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href={import.meta.env.BASE_URL + "/resume"}
+              >
+                Resume
+              </a>
             </li>
-
-            <li>
-              <a href="#AboutMe">About</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#AboutMe">
+                About
+              </a>
             </li>
-
-            <li>
-              <a href="#Contact">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#Contact">
+                Contact
+              </a>
             </li>
+            {/* <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
+            </li> */}
           </ul>
-        </nav>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 }
-
 export default NavBar;
